8253fd4a0375a8267e656853c77559afc4ebe15b
jesus <jesus_lopez1997@hotmail.com>
2020-08-01 02:17:46 -0400

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,63 @@
+
+
+
+const tile_1 = document.querySelector("#tile_1");
+tile_1.addEventListener("click", () => {
+  console.log("tile 1 clicked")
+});
+
+
+const tile_2 = document.querySelector("#tile_2");
+tile_2.addEventListener("click", () => {
+  console.log("tile 2 clicked")
+});
+
+
+const tile_3 = document.querySelector("#tile_3");
+tile_3.addEventListener("click", () => {
+  console.log("tile 3 clicked")
+});
+
+const tile_4 = document.querySelector("#tile_4");
+tile_4.addEventListener("click", () => {
+  console.log("tile 4 clicked")
+});
+
+const tile_5 = document.querySelector("#tile_5");
+tile_5.addEventListener("click", () => {
+  console.log("tile 5 clicked")
+});
+
+const tile_6 = document.querySelector("#tile_6");
+tile_6.addEventListener("click", () => {
+  console.log("tile 6 clicked")
+});
+
+const tile_7 = document.querySelector("#tile_7");
+tile_7.addEventListener("click", () => {
+  console.log("tile 7 clicked")
+});
+
+const tile_8 = document.querySelector("#tile_8");
+tile_8.addEventListener("click", () => {
+  console.log("tile 8 clicked")
+});
+
+const tile_9 = document.querySelector("#tile_9");
+tile_9.addEventListener("click", () => {
+  console.log("tile 9 clicked")
+});
+
+const board = {
+  tile_1: false,
+  tile_2: false,
+  tile_3: false,
+  tile_4: false,
+  tile_5: false,
+  tile_6: false,
+  tile_7: false,
+  tile_8: false,
+  tile_9: false,
+}
+
+console.log(board);
